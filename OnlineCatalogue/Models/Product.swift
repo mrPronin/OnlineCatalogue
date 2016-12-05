7e19a702c50861d00a3193125c56b9aad41e739b
Alexander Pronin <pronin.alx@gmail.com>
2016-12-06 01:55:13 +0200

    Initial commit

---
+++ OnlineCatalogue/Models/Product.swift
@@ -0,0 +1,27 @@
+//
+//  Product.swift
+//  OnlineCatalogue
+//
+//  Created by Aleksandr Pronin on 12/6/16.
+//  Copyright © 2016 Aleksandr Pronin. All rights reserved.
+//
+
+import Foundation
+
+struct Product: Equatable
+{
+    var id: String?
+    var site_id: String?
+    var title: String?
+    var price: NSDecimalNumber?
+    var currency_id: String?
+    var thumbnail: String?
+}
+
+func ==(lhs: Product, rhs: Product) -> Bool
+{
+    return lhs.id == rhs.id
+        && lhs.site_id == rhs.site_id
+        && lhs.title == rhs.title
+        && lhs.currency_id == rhs.currency_id
+}
---
+++ OnlineCatalogue/Services/ProductsCoreDataStore.swift
@@ -0,0 +1,9 @@
+//
+//  ProductsCoreDataStore.swift
+//  OnlineCatalogue
+//
+//  Created by Aleksandr Pronin on 12/6/16.
+//  Copyright © 2016 Aleksandr Pronin. All rights reserved.
+//
+
+import Foundation
